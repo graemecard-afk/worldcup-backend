4b24131eb17761f77be8c70b3683326e43974fe3
graemecard-afk <graeme.card@gmail.com>
2025-12-11 20:52:12 +1300

    Enable SSL for Postgres on Render

--- db.js
+++ db.js
@@ -7,6 +7,7 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 export async function query(text, params) {
